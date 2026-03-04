2c48107f322e5905c016ff10482d07ecae46905a
Deathlesz <deathless.mcd@gmail.com>
2026-03-04 13:53:28 +0300

    fix cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.router import api_router
 from app.core.config import get_settings
@@ -19,6 +20,22 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(title=settings.app_name, version="0.1.0", lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://formatis.online",
+        "https://mts-hackathon.vercel.app",
+        "http://localhost",
+        "http://localhost:80",
+        "http://localhost:8000",
+        "http://127.0.0.1",
+        "http://127.0.0.1:80",
+        "http://127.0.0.1:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix=settings.api_prefix)
 
 
